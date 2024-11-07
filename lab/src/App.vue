481b17625e9c684ff42bc0fc5ece7d1af919504f
wyh <251793636@qq.com>
2024-11-07 11:11:56 +0800

    3.3

--- lab/src/App.vue
+++ lab/src/App.vue
@@ -25,13 +25,6 @@ const { message } = storeToRefs(store)
 </template>
 
 <style>
-#layout {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
 nav {
   padding: 30px;
